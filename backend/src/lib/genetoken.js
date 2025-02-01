7408848270a3ca5fe760e02a2938c441e3ea9a82
Garvyadav888 <garv4072@gmail.com>
2025-02-01 16:40:12 +0530

    .

--- backend/src/lib/genetoken.js
+++ backend/src/lib/genetoken.js
@@ -6,11 +6,12 @@ const generatetoken = (userid, res) => {
 	const token = jsonwebtoken.sign({ userid }, process.env.JWTOKEN, {
 		expiresIn: '7d',
 	});
+	console.log(process.env.JWTOKEN);
 	res.cookie('token', token, {
 		httpOnly: true,
 		maxAge: 604800000,
 		sameSite: 'strict',
-		secure: process.env.NODE_ENV !== 'development',
+		secure: 'production',
 	});
 	return token;
 };
