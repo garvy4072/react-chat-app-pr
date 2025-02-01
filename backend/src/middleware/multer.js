36bc372f03d7ce42a85dcd21bf6944635f53d5a1
Garvyadav888 <garv4072@gmail.com>
2025-02-01 12:42:27 +0530

    .

--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -1,18 +1,1 @@
 /** @format */
-
-import multer from 'multer';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, path.join(__dirname, './upload'));
-	},
-	filename: (req, file, cb) => {
-		console.log(file);
-		const ext = path.extname(file.originalname);
-		const filename = file.originalname.replace(ext, '');
-		cb(null, `${filename}${Date.now()}${ext}`);
-	},
-});
-const uploader = multer({ storage: storage });
-export default uploader;
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -7,10 +7,9 @@ import {
 	getmessage,
 	sendmessage,
 } from '../controllers/message.controller.js';
-import uploader from '../middleware/multer.js';
 
 const router = e.Router();
 router.get('/users', protectroute, getuserforsidbar);
 router.get('/:id', protectroute, getmessage);
-router.post('/send/:id', uploader.single('image'), protectroute, sendmessage);
+router.post('/send/:id', protectroute, sendmessage);
 export default router;
