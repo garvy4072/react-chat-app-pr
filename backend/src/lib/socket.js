94a76454332f86ae93361bf6a3ee8a4cff20af5e
Garvyadav888 <garv4072@gmail.com>
2025-02-01 16:09:29 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,14 +9,6 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		origin: 'http://localhost:5173',
-		methods: ['GET', 'POST', 'PUT', 'DELETE'],
-		allowedHeaders: [
-			'Content-Type',
-			'Authorization',
-			'Cache-Control',
-			'Expires',
-			'pragma',
-		],
 		credentials: true,
 	},
 });
